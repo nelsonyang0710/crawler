4dcaf655f5e9ea76a408fe8ab1be428b0e741be7
nelsonyang0710 <nelsonyang0710@yahoo.com>
2015-09-18 18:22:39 -0700

    done.

--- CurlWorker.php
+++ CurlWorker.php
@@ -8,33 +8,169 @@
  */
 class CurlWorker
 {
-    private $curl_info;
-    private $base_url;
-    private $depth = 0;
-    private static $instance;
-    const MAX_DEPTH = 1;
-    private function __construct($url)
-    {
-        $this->base_url = $url;
+    private $curl_info = array();
+    private $curl_error_count = 0;
+    private $request_url = null;
+    private $base_url = null;
+    private $request_uri = null;
 
-    }
-    public static function getInstance($url = null)
+    /**
+     * @param $url
+     * @throws Exception
+     */
+    public function __construct($url)
     {
-        if (null === static::$instance) {
-            static::$instance = new static($url);
-        }
-        return static::$instance;
+        $this->filterUrl($url);
     }
-    public function curl()
+
+    /**
+     * @param null $url
+     * @throws Exception
+     */
+    public function curl($url = null)
     {
         $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $this->url);
+        if ($url)
+        {
+            curl_setopt($ch, CURLOPT_URL, $url);
+        }
+        else
+        {
+            curl_setopt($ch, CURLOPT_URL, $this->request_url);
+        }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //to handle redirect request
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $info = curl_getinfo($ch);
-
-        curl_close($ch);
+        //to handle https request
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $content = curl_exec($ch);
+        if(!curl_errno($ch))
+        {
+            $info = curl_getinfo($ch);
+            curl_close($ch);
+            if ($url)
+            {
+                if (strpos($info['content_type'],'html') !== false)
+                {
+                    $this->curl_info['html']['count'] ++;
+                    $this->curl_info['html']['size'] += $info['size_download'];
+                }
+                else if (strpos($info['content_type'],'image') !== false)
+                {
+                    $this->curl_info['images']['count'] ++;
+                    $this->curl_info['images']['size'] += $info['size_download'];
+                }
+                else if (strpos($info['content_type'],'css') !== false)
+                {
+                    $this->curl_info['css']['count'] ++;
+                    $this->curl_info['css']['size'] += $info['size_download'];
+                }
+                else if (strpos($info['content_type'],'javascript') !== false)
+                {
+                    $this->curl_info['javascript']['count'] ++;
+                    $this->curl_info['javascript']['size'] += $info['size_download'];
+                }
+                else if (!empty($this->curl_info[$info['content_type']]))
+                {
+                    $this->curl_info[$info['content_type']]['count'] ++;
+                    $this->curl_info[$info['content_type']]['size'] += $info['size_download'];
+                }
+                else
+                {
+                    $this->curl_info[$info['content_type']] = array('count' => 1, 'size' => $info['size_download']);
+                }
+            }
+            else
+            {
+                if (strpos($info['content_type'],'html') !== false)
+                {
+                    $this->curl_info['html'] = array('count' => 1, 'size' => $info['size_download']);
+                    $this->curl_info['images'] = array('count' => 0, 'size' => 0);
+                    $this->curl_info['css'] = array('count' => 0, 'size' => 0);
+                    $this->curl_info['javascript'] = array('count' => 0, 'size' => 0);
+                    preg_match_all('/(href|src)=["\'](\S*)["\']/', $content, $matches);
+                    $unique_result = array_unique($matches[2]);
+                    foreach ($unique_result as $html_url)
+                    {
+                        $html_url = $this->filterUrl($html_url);
+                        if ($html_url != $this->request_url && !empty($html_url))
+                        {
+                            $this->curl($html_url);
+                        }
+                    }
+                }
+                else
+                {
+                    $this->curl_info[$info['content_type']] = array('count' => 1, 'size' => $info['size_download']);
+                }
+            }
+        }
+        else
+        {
+            $this->curl_error_count++;
+            curl_close($ch);
+        }
     }
 
+    /**
+     * @return array
+     */
+    public function getResult()
+    {
+        return $this->curl_info;
+    }
+
+    /**
+     * @param $url
+     * @return string
+     * @throws Exception
+     */
+    private function filterUrl($url)
+    {
+        if (!empty($url))
+        {
+            $url_info = parse_url($url);
+            if (is_null($this->request_url) || is_null($this->base_url))
+            {
+                if (empty($url_info['host']))
+                {
+                    throw new Exception('You have to provide a valid Url.');
+                }
+                else
+                {
+                    $this->base_url = (empty($url_info['schema']) ? 'http://' : $url_info['schema'] . '://') . $url_info['host'] ;
+                    $this->request_uri = $this->base_url .(empty($url_info['path']) ? '' : $url_info['path']);
+                    $this->request_url = $this->request_uri . (empty($url_info['query']) ? '' : '?' .$url_info['query']);
+                }
+            }
+            else
+            {
+                if (!empty($url_info['host']))
+                {
+                    return (empty($url_info['schema']) ? 'http://' : $url_info['schema'] . '://') . $url_info['host'] . (empty($url_info['path']) ? '' : $url_info['path']) . (empty($url_info['query']) ? '' : '?' .$url_info['query']);
+                }
+                else if (!empty($url_info['path']))
+                {
+                    if (strpos($url_info['path'],'/') === 0)
+                    {
+                        return $this->base_url . (empty($url_info['path']) ? '' : $url_info['path']) . (empty($url_info['query']) ? '' : '?' .$url_info['query']);
+                    }
+                    else
+                    {
+                        return rtrim($this->request_uri, '/') . '/' . $url_info['path'] . (empty($url_info['query']) ? '' : '?' . $url_info['query']);
+                    }
+                }
+            }
+        }
+        //for invalid path, return empty stating instead of throw exception
+        return '';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHost()
+    {
+        return parse_url($this->request_url, PHP_URL_HOST);
+    }
 }
--- crawler.php
+++ crawler.php
@@ -7,5 +7,39 @@
  */
 require_once("CurlWorker.php");
 $url = $argv[1];
-CurlWorker::getInstance($url)->cure();
-$result = CurlWorker::getInstance()->getResult();
+$url = 'http://www.care2.com';
+try
+{
+    $curl_worker = new CurlWorker($url);
+    $curl_worker->curl();
+}
+catch (Exception $e)
+{
+    echo $e->getMessage();
+    die();
+}
+$result = $curl_worker->getResult();
+$total_request_count = 0;
+$total_download_size = 0;
+$host = $curl_worker->getHost();
+foreach ($result as $type)
+{
+    $total_request_count += $type['count'];
+    $total_download_size += $type['size'];
+}
+$total_request_count = number_format($total_request_count);
+$total_download_size = number_format($total_download_size);
+
+echo "Processed URL : $host\n\n";
+
+echo "Total Requests : $total_request_count\n";
+echo "Total Size  : $total_download_size bytes\n";
+
+echo "By Content Type\n";
+foreach ($result as $type => $type_info)
+{
+    $uc_type = ucwords($type);
+    $request_count = number_format($type_info['count']);
+    $request_size = number_format($type_info['size']);
+    echo "$uc_type : $request_count requests, $request_size bytes\n";
+}
